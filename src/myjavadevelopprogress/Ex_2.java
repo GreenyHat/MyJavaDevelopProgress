96f052af63037636d54d00fb1e2841833c964b19
GreenyHat <aramirezgarcia95@gmail.com>
2022-10-10 18:46:05 +0200

    initial-state

---
+++ src/myjavadevelopprogress/Ex_2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myjavadevelopprogress;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Greeny
+ */
+public class Ex_2 {
+    ////////////////////*COFFE SHOP:*////////////////////////
+    
+public static void main(String[] args){   
+    
+  Scanner sc = new Scanner(System.in); 
+  
+    int coffeCons = sc.nextInt(),toastCons = sc.nextInt(), juiceCons = sc.nextInt();//this variables will save the consumed products.
+    
+    String terminalBarman="What Can I help you?";
+    
+    System.out.print();
+    
+   
+    
+    
+}
+
+}
