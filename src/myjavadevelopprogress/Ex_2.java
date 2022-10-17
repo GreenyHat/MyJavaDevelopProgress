90ef5614b1fa91dd494f9cc2c89a53833e4fed7e
GreenyHat <antonioenriqueramirez1@gmail.com>
2022-10-17 13:02:07 +0200

    exercices(3-4)-added

--- src/myjavadevelopprogress/Ex_2.java
+++ src/myjavadevelopprogress/Ex_2.java
@@ -15,7 +15,7 @@ public class Ex_2 {
     
 public static void main(String[] args){   
     int coffeNum, toastNum, juiceNum;
-    double coffePrize = 1.1, toastPrize = 1.5, juicePrize = 2, cuenta = 0;
+    double coffePrize = 1.1, toastPrize = 1.5, juicePrize = 2, account = 0;
     Scanner scKey = new Scanner(System.in);
   
     System.out.println("How many coffees are you going to drink?");
@@ -27,12 +27,26 @@ public static void main(String[] args){
    System.out.println("Any juice?");
    juiceNum = scKey.nextInt();
    
-   cuenta = (coffePrize*coffeNum) + (juiceNum*juicePrize) + (toastNum*toastPrize);
-   System.out.println("Well, the account would be: " + cuenta + "$");
+   account = (coffePrize*coffeNum) + (juiceNum*juicePrize) + (toastNum*toastPrize);
+    
+    if (account==0) {
+        
+        System.out.println("Get out of my fucking bar!");
+   
+        
+    }
     
    
+    else {
     
+    System.out.println("Thank you, your account would be: " + account + "$");
+    
+    
+
+    
+    }
+    
+    }
     
 }
 
-}
---
+++ src/myjavadevelopprogress/Ex_3.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myjavadevelopprogress;
+import java.util.Scanner;
+
+/**
+ *
+ * @author greeny
+ */
+ /////////////////////AN ABSURD MODEL FOR THE HOROSCOPE/////////////////////////
+
+public class Ex_3 {
+    
+public static void main(String[] args){  
+    int dayBorn, monthBorn;
+    String stupidHoroscope = null;
+    
+    Scanner scKey = new Scanner(System.in);
+    
+    System.out.println("What day were you born?");    
+    dayBorn = scKey.nextInt();
+    System.out.println("and what month?, please introduce one or two numbers, my level of programming does not allow me another solution ");
+    monthBorn = scKey.nextInt();
+    
+    ///////////ARIES////////////////
+    
+    if (dayBorn >=21 && dayBorn <= 31 && monthBorn == 3 ) {
+        stupidHoroscope = "Your horoscope is Aries";
+        System.out.println(stupidHoroscope);
+    }
+    
+    
+     else if (dayBorn<=20&&monthBorn==4) {
+        stupidHoroscope="Your horoscope is Aries";
+          System.out.println(stupidHoroscope);
+     }
+                
+///////////////////////////////////////////////////////////
+
+      if (dayBorn >=21 && dayBorn<=28&&monthBorn==4) {
+     stupidHoroscope = "Your horoscope is Tauro";
+          System.out.println(stupidHoroscope);
+             
+             }
+      else if (dayBorn >=21 && dayBorn <= 31 && monthBorn == 5) {
+        stupidHoroscope = "Your horoscope is Tauro";
+        System.out.println(stupidHoroscope);
+        
+    }
+   
+}
+}
+
+
