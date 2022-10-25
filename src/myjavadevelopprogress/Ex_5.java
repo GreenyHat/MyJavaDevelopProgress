6b7706e3cbbc41ca15b8ce7c967f4c07418b35d7
GreenyHat <aramirezgarcia95@gmail.com>
2022-10-25 20:41:48 +0200

    Ex_6-added

--- src/myjavadevelopprogress/Ex_5.java
+++ src/myjavadevelopprogress/Ex_5.java
@@ -1,5 +1,6 @@
 package myjavadevelopprogress;
 
+
 /**
  *
  * @author Greeny
@@ -9,10 +10,27 @@ package myjavadevelopprogress;
     public class Ex_5 {
         ///////////////AQUI EMPIEZAN EJERCICIOS DE PÍLDORAS//////////////////
         
-     public static void main(String[] args){ 
+    public static void main(String[] args){ 
+        Double numero = 0.0, resultado=0.0;
+        Scanner scKey = new Scanner(System.in);
+        Scanner sckey2 = new Scanner(System.in);
+        System.out.println("Dame un número para calcular raíz cuadrada: ");
+        
+        numero = scKey.nextDouble();
+        resultado = Math.sqrt(numero);
 
+        System.out.println("El resultado de la operación es: " + resultado +
+         " que es la raíz cuadrada de "+ numero);
 
-     }
+         System.out.println("Intruduce el número que quieres elevar y luego el exponente: ");
+         double base,exponente,resultadoPotencia;
+
+         base = sckey2.nextDouble();
+         exponente = sckey2.nextDouble();
+         resultadoPotencia = Math.pow(base,exponente);
+         System.out.println("El resultado de " + base +" elevado a "+ exponente + "da : "+resultadoPotencia);
+     }   
+
     
     
  }
---
+++ src/myjavadevelopprogress/Ex_6.java
@@ -0,0 +1,10 @@
+package myjavadevelopprogress;
+
+public class Ex_6 {
+
+
+    public static void main(String[]args){
+        
+    }
+    
+}
