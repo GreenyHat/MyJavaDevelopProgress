79195c4cd47b24435de56513334391cb05b9301c
GreenyHat <aramirezgarcia95@gmail.com>
2023-06-07 13:45:00 +0200

    fast-update

--- src/myjavadevelopprogress/myjavadevelopprogress/pildoras_informaticas/POO/Ex_8_media_13_ATS.java
+++ src/myjavadevelopprogress/myjavadevelopprogress/pildoras_informaticas/POO/Ex_8_media_13_ATS.java
@@ -3,32 +3,26 @@ package myjavadevelopprogress.pildoras_informaticas.POO;
 import javax.swing.JOptionPane;
 
 public class Ex_8_media_13_ATS {
-    
-public static void main(String[] args) {
-    int number, positives = 0, negatives = 0, zeroCount = 0;
-    int panel = Integer.parseInt(JOptionPane.showInputDialog("give me ten numbers:"));
 
+    public static void main(String[] args) {
+        int number, positives = 0, negatives = 0, zeroCount = 0;
 
-do {
+        
 
-    for (int numbers = 0; numbers < 10; numbers++) {
-       if (panel>0) {
-        positives++;
-       }else if (panel==0) {
-        zeroCount++;
-       }else{
-         negatives++;
-       }
-       System.out.println("La media de los positivos es : ");
+            for (int numbers = 0; numbers <= 10; numbers++) {
+                number = Integer.parseInt(JOptionPane.showInputDialog("give me a numbers:"));
+                if (panel > 0) {
+                    positives++;
+                } else if (panel == 0) {
+                    zeroCount++;
+                } else {
+                    negatives++;
+                }
+                System.out.println("La media de los positivos es : ");
+            }
+
+        
+
     }
-
-} while (panel<=10);
-
-
-   
-    }
-    
 
 }
-
-
