6249d9b5152bc226ffe3dc0a05058d1ea8261351
GreenyHat <aramirezgarcia95@gmail.com>
2023-06-15 20:11:56 +0200

    excercises-fixedAndAdded

--- src/myjavadevelopprogress/myjavadevelopprogress/pildoras_informaticas/POO/TestingClasses.java
+++ src/myjavadevelopprogress/myjavadevelopprogress/pildoras_informaticas/POO/TestingClasses.java
@@ -5,7 +5,8 @@ public class TestingClasses {
     private String propertyString;
     private double propertyDouble;
 
-    public TestingClasses(){//CONSTRUCTOR de la clase mediante el que se crean objetos de la clase
+    public TestingClasses() {// CONSTRUCTOR de la clase mediante el que se crean objetos de la clase, suele
+                             // tener el mismo nombre que la clase
 
         propertyInt = 3;
         propertyDouble = 2.3;
@@ -13,29 +14,29 @@ public class TestingClasses {
 
     }
 
-    public void setPropertyString(){//metodo setter establece valor de propiedad
-        propertyString = "defined";//es privado pero accedemos con el metodo setter para ESTABLECER UN VALOR
+    public void setPropertyString() {// metodo setter establece valor de propiedad
+        propertyString = "defined";// es privado pero accedemos con el metodo setter para ESTABLECER UN VALOR
     }
 
-    public void setPropertyDouble(){
+    public void setPropertyDouble() {
         propertyDouble = 5.6;
     }
 
-    public void setPropertyInt(){
+    public void setPropertyInt() {
         propertyInt = 2;
     }
 
-    public String getPropString(){//metodo getter devuelve valor de propiedad
+    public String getPropString() {// metodo getter devuelve valor de propiedad
         return propertyString;
-        
+
     }
 
-    public double getPropertyDouble(){
+    public double getPropertyDouble() {
         return propertyDouble;
     }
 
-    public int getPropertyInt(){
+    public int getPropertyInt() {
         return propertyInt;
     }
-    
+
 }
